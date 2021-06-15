c3df07732290eb372ac06387ddb944d03ca23d13
indraabhushan.pr <68438304+indru-dudez@users.noreply.github.com>
2021-06-15 20:33:08 +0530

    Update convert_to_video.py

--- plugins/convert_to_video.py
+++ plugins/convert_to_video.py
@@ -136,7 +136,7 @@ async def convert_to_video(bot, update):
             except:
                 pass
             await bot.edit_message_text(
-                text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
+                text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG2,
                 chat_id=update.chat.id,
                 message_id=a.message_id,
                 disable_web_page_preview=True
